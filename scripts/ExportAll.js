25118744b77e45f96912cff013ca7295feca07fa
Chris <chrisbotanic@outlook.com.ar>
2019-10-10 18:29:12 -0300

    Add export via command line

---
+++ scripts/ExportAll.js
@@ -0,0 +1,26 @@
+const db = require('../domain/services/database.js');
+const { Bot, BotSubscription } = require('../domain/greenhouse/entities/Bot');
+
+async function main() {
+  await db();
+  process.stdout.write(`Exporting from ${process.env.GREENHOUSE_DATABASE} to ${process.env.RHIZOME_DATABASE} ...`);
+
+  // Export greenhouse_dotbots
+  const allBots = await Bot.find().exec();
+  const botPromises = allBots.map(async (bot) => {
+    await bot.save();
+  });
+  await Promise.all(botPromises);
+
+  // Export greenhouse_publisher_bots
+  const allBotSubscriptions = await BotSubscription.find().exec();
+  const botSubscriptionsPromises = allBotSubscriptions.map(async (botSubscription) => {
+    await botSubscription.save();
+  });
+  await Promise.all(botSubscriptionsPromises);
+}
+
+main().then(() => {
+  console.log('done.\n');
+  process.exit();
+});
