203d2226ea29a2125fdc67f0e2227577fdce3721
Chris <chrisbotanic@outlook.com.ar>
2019-11-15 20:40:06 -0300

    Add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'greenhouse',
+    script: './server/bin/www.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    node_args: '-r dotenv/config',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G'
+  }],
+
+  deploy : {
+  }
+};
